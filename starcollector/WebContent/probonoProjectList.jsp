2cf194129262151b7c6f0220e216592eaf6a1514
ArkimCity <jaeung949@gmail.com>
2020-12-26 12:32:54 +0900

    real init

---
+++ starcollector/WebContent/probonoProjectList.jsp
@@ -0,0 +1,45 @@
+﻿<%@ page language="java" contentType="text/html; charset=utf-8"
+	pageEncoding="utf-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%
+	String url = application.getContextPath() + "/";
+%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>모든 Probono Project list 화면</title>
+</head>
+<body>
+	<br>
+	<br>
+	<br>
+	<center>
+		<h3>재능 기부 프로젝트 list</h3>
+		<hr>
+		<p>
+		<table border="1">
+			<tr>
+				<th>프로젝트 id</th>
+				<th>프로젝트명</th>
+				<th>재능기부자 id</th>
+				<th>재능 수혜자 id</th>
+				<th>재능기부 내용</th>
+			</tr>
+			<c:forEach items="${requestScope.probonoProjectAll}" var="pp">
+				<tr>
+					<td>${pp.probonoProjectId}</td>
+					<td>${pp.probonoProjectName}</td>
+					<td><button onclick="location.href='probono?command=activist&activistId=${pp.activistId.activistId}'">${pp.activistId.activistId}</button></td>
+					<td><button onclick="location.href='probono?command=recipient&recipientId=${pp.recipientId.recipientId}'">${pp.recipientId.recipientId}</button></td>
+					<td>${pp.projectContent}</td>
+				</tr>
+			</c:forEach>
+		</table>
+		<br>
+		<br>
+		<br> <font color="blue">재능 기부자 id 및 재능 수혜자 id를 클릭하면 상세 정보
+			확인이 가능합니다</font> <a href="index.html">메인 화면 이동</a>
+	</center>
+</body>
+</html>
---
+++ starcollector/src/probono/model/ProbonoProjectDAO.java
@@ -0,0 +1,158 @@
+﻿/*
+CREATE TABLE probono_project (
+	   probono_project_id     		NUMBER(5) PRIMARY KEY,
+	   probono_project_name 		VARCHAR2(50) NOT NULL,
+       probono_id           			VARCHAR2(50) NOT NULL,       
+       activist_id          				VARCHAR2(20) NOT NULL,
+       receive_id           				VARCHAR2(20) NOT NULL, 
+       project_content      			VARCHAR2(100) NOT NULL
+);   */
+package probono.model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.persistence.EntityManager;
+
+import probono.model.dto.ActivistDTO;
+import probono.model.dto.ProbonoDTO;
+import probono.model.dto.ProbonoProjectDTO;
+import probono.model.dto.RecipientDTO;
+import probono.model.util.DBUtil;
+import probono.model.util.PublicCommon;
+
+public class ProbonoProjectDAO {
+	
+	//프로보노 프로젝트 저장
+	public static boolean addProbonoProject(ProbonoProjectDTO probonoProject) throws SQLException{
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		try{
+			con = DBUtil.getConnection();
+			pstmt = con.prepareStatement("insert into probono_project values(probono_project_id_seq.nextval, ? ?, ?, ?, ?)");
+			pstmt.setString(1, probonoProject.getProbonoProjectName());
+			pstmt.setString(2, probonoProject.getProbonoId().getProbonoId());
+			pstmt.setString(3, probonoProject.getActivistId().getActivistId());
+			pstmt.setString(4, probonoProject.getRecipientId().getRecipientId());
+			pstmt.setString(5, probonoProject.getProjectContent());
+			
+			int result = pstmt.executeUpdate();
+		
+			if(result == 1){
+				return true;
+			}
+		}finally{
+			DBUtil.close(con, pstmt);
+		}
+		return false;
+	}
+	
+	//수정
+	// 프로보노 프로젝트 ID로  재능기부자 수정
+	public static boolean updateProbonoProjectActivist(int projectId, String activistId) throws SQLException{		
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		try{
+			con = DBUtil.getConnection();
+			
+			pstmt = con.prepareStatement("update probono_project set activist_id=? where probono_project_id=?");
+			pstmt.setString(1, activistId);
+			pstmt.setInt(2, projectId);
+			
+			int result = pstmt.executeUpdate();
+			if(result == 1){
+				return true;
+			}
+		}finally{
+			DBUtil.close(con, pstmt);
+		}
+		return false;
+	}
+	
+	//수정
+	//프로보노 프로젝트 id로 수해자 정보 변경
+	public static boolean updateProbonoProjectReceive(int probonoProjectId, String  receiveId) throws SQLException{
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		try{
+			con = DBUtil.getConnection();
+			
+			pstmt = con.prepareStatement("update probono_project set receive_id=? where probono_project_id=?");
+			pstmt.setString(1, receiveId);
+			pstmt.setInt(2, probonoProjectId);
+			
+			int result = pstmt.executeUpdate();
+			if(result == 1){
+				return true;
+			}
+		}finally{
+			DBUtil.close(con, pstmt);
+		}
+		return false;
+	}
+	
+	public static boolean deleteProbonoProject(int probonoProjectId) throws SQLException{
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		try{
+			con = DBUtil.getConnection();
+			pstmt = con.prepareStatement("delete from probono_project where probono_project_id=?");
+			pstmt.setInt(1, probonoProjectId);
+			int result = pstmt.executeUpdate();
+			if(result == 1){
+				return true;
+			}
+		}finally{
+			DBUtil.close(con, pstmt);
+		}
+		return false;
+	}
+	
+	//프로보노 프로젝트 id로 해당 프로보노프로젝트 검색
+	public static ProbonoProjectDTO getProbonoProject(int probonoProjectId) throws SQLException{
+		EntityManager em = PublicCommon.getEntityManger();
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		ProbonoProjectDTO probonoUser = null;
+		
+		try{
+			con = DBUtil.getConnection();
+			pstmt = con.prepareStatement("select * from probono_project where probono_project_id=?");
+			pstmt.setInt(1, probonoProjectId);
+			rset = pstmt.executeQuery();
+			if(rset.next()){
+				probonoUser = ProbonoProjectDTO.builder().probonoProjectId(rset.getInt(1)).probonoProjectName(rset.getString(2)).probonoId(em.find(ProbonoDTO.class, rset.getString(3)))
+						.activistId(em.find(ActivistDTO.class, rset.getString(4))).recipientId(em.find(RecipientDTO.class, rset.getString(5))).projectContent(rset.getString(6)).build();
+						}
+		}finally{
+			DBUtil.close(con, pstmt, rset);
+		}
+		return probonoUser;
+	}
+	
+	//모든 프로보노 프로젝트 검색 
+	public static ArrayList<ProbonoProjectDTO> getAllProbonoProjects(EntityManager em) throws SQLException{
+		Connection con = null; 
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		ArrayList<ProbonoProjectDTO> list = null;
+		try{
+			con = DBUtil.getConnection();
+			pstmt = con.prepareStatement("select * from probono_project");
+			rset = pstmt.executeQuery();
+			
+			list = new ArrayList<ProbonoProjectDTO>();
+			while(rset.next()){
+				list.add(ProbonoProjectDTO.builder().probonoProjectId(rset.getInt(1)).probonoProjectName(rset.getString(2)).probonoId(em.find(ProbonoDTO.class, rset.getString(3)))
+						.activistId(em.find(ActivistDTO.class, rset.getString(4))).recipientId(em.find(RecipientDTO.class, rset.getString(5))).projectContent(rset.getString(6)).build());
+			}
+		}finally{
+			DBUtil.close(con, pstmt, rset);
+		}
+		return list;
+	}
+}
